8fe4118034c47f45e4dc86e0e33d94e8c00c8f93
Audrey Schmidt <audsch@outlook.com>
2022-02-07 10:17:26 -0600

    feat: added authentication app using User model from django for user login functions

--- assignmentdashboard/assignmentdashboard/settings.py
+++ assignmentdashboard/assignmentdashboard/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'assignment.apps.AssignmentConfig'
+    'assignment.apps.AssignmentConfig',
+    'authentication.apps.AuthenticationConfig'
 ]
 
 MIDDLEWARE = [
